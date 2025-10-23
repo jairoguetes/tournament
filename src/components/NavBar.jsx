e222f3f50f6574347e405bdd131bad8dfdf27cd0
jairoguetes <jairojrguetes@gmail.com>
2025-10-22 19:13:03 -0500

    Changed: the import method

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import narutoNavbarImage from '../assets/images/narutoNavBar.png'; 
 import "./components_styles/NavBar.css";
+
+const navbarStyle = {
+    height: '150px',
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${narutoNavbarImage})`,
+    
+   
+};
 
 export const NavBar = () => {
     return (
-        <section className="navbar-background">
+        <section className="navbar-background" style={navbarStyle}>
             <p className='navBarTitle'>Naruto</p>
             <br />
             <p className='navBarTitle'>Tournament</p>
